5ce5e831bf12c23860b92a675e0742d959fef11f
Eric Muyser <eric@muyser.com>
2013-02-20 08:40:00 +0900

    Parameter serialization should work for multiple select boxes.
    * Add $.deparam dependency.
    * Fix $.cookie spacing.

--- frontend.js
+++ frontend.js
@@ -5,47 +5,141 @@
  * the MooTools AJAX implementation. Credit: stilbuero for $.cookie plugin.
  */
 
+(function ($) {
+  $.deparam = function (params, coerce) {
+    var obj = {},
+        coerce_types = { 'true': !0, 'false': !1, 'null': null };
+      
+    // Iterate over all name=value pairs.
+    $.each(params.replace(/\+/g, ' ').split('&'), function (j,v) {
+      var param = v.split('='),
+          key = decodeURIComponent(param[0]),
+          val,
+          cur = obj,
+          i = 0,
+            
+          // If key is more complex than 'foo', like 'a[]' or 'a[b][c]', split it
+          // into its component parts.
+          keys = key.split(']['),
+          keys_last = keys.length - 1;
+        
+      // If the first keys part contains [ and the last ends with ], then []
+      // are correctly balanced.
+      if (/\[/.test(keys[0]) && /\]$/.test(keys[keys_last])) {
+        // Remove the trailing ] from the last keys part.
+        keys[keys_last] = keys[keys_last].replace(/\]$/, '');
+          
+        // Split first keys part into two parts on the [ and add them back onto
+        // the beginning of the keys array.
+        keys = keys.shift().split('[').concat(keys);
+          
+        keys_last = keys.length - 1;
+      } else {
+        // Basic 'foo' style key.
+        keys_last = 0;
+      }
+        
+      // Are we dealing with a name=value pair, or just a name?
+      if (param.length === 2) {
+        val = decodeURIComponent(param[1]);
+          
+        // Coerce values.
+        if (coerce) {
+          val = val && !isNaN(val)              ? +val              // number
+              : val === 'undefined'             ? undefined         // undefined
+              : coerce_types[val] !== undefined ? coerce_types[val] // true, false, null
+              : val;                                                // string
+        }
+          
+        if ( keys_last ) {
+          // Complex key, build deep object structure based on a few rules:
+          // * The 'cur' pointer starts at the object top-level.
+          // * [] = array push (n is set to array length), [n] = array if n is 
+          //   numeric, otherwise object.
+          // * If at the last keys part, set the value.
+          // * For each keys part, if the current level is undefined create an
+          //   object or array based on the type of the next keys part.
+          // * Move the 'cur' pointer to the next level.
+          // * Rinse & repeat.
+          for (; i <= keys_last; i++) {
+            key = keys[i] === '' ? cur.length : keys[i];
+            cur = cur[key] = i < keys_last
+              ? cur[key] || (keys[i+1] && isNaN(keys[i+1]) ? {} : [])
+              : val;
+          }
+            
+        } else {
+          // Simple key, even simpler rules, since only scalars and shallow
+          // arrays are allowed.
+            
+          if ($.isArray(obj[key])) {
+            // val is already an array, so push on the next value.
+            obj[key].push( val );
+              
+          } else if (obj[key] !== undefined) {
+            // val isn't an array, but since a second value has been specified,
+            // convert val into an array.
+            obj[key] = [obj[key], val];
+              
+          } else {
+            // val is a scalar.
+            obj[key] = val;
+          }
+        }
+          
+      } else if (key) {
+        // No value was defined, so set something meaningful.
+        obj[key] = coerce
+          ? undefined
+          : '';
+      }
+    });
+      
+    return obj;
+  };
+})(jQuery);
+
 (function($) {
 	jQuery.cookie = function(name, value, options) {
-			if (typeof value != 'undefined') { // name and value given, set cookie
-					options = options || {};
-					if (value === null) {
-							value = '';
-							options.expires = -1;
-					}
-					var expires = '';
-					if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
-							var date;
-							if (typeof options.expires == 'number') {
-									date = new Date();
-									date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-							} else {
-									date = options.expires;
-							}
-							expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
-					}
-					// CAUTION: Needed to parenthesize options.path and options.domain
-					// in the following expressions, otherwise they evaluate to undefined
-					// in the packed version for some reason...
-					var path = options.path ? '; path=' + (options.path) : '';
-					var domain = options.domain ? '; domain=' + (options.domain) : '';
-					var secure = options.secure ? '; secure' : '';
-					document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-			} else { // only name given, get cookie
-					var cookieValue = null;
-					if (document.cookie && document.cookie != '') {
-							var cookies = document.cookie.split(';');
-							for (var i = 0; i < cookies.length; i++) {
-									var cookie = jQuery.trim(cookies[i]);
-									// Does this cookie string begin with the name we want?
-									if (cookie.substring(0, name.length + 1) == (name + '=')) {
-											cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-											break;
-									}
-							}
-					}
-					return cookieValue;
+		if (typeof value != 'undefined') { // name and value given, set cookie
+			options = options || {};
+			if (value === null) {
+				value = '';
+				options.expires = -1;
 			}
+			var expires = '';
+			if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+				var date;
+				if (typeof options.expires == 'number') {
+					date = new Date();
+					date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+				} else {
+					date = options.expires;
+				}
+				expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+			}
+			// CAUTION: Needed to parenthesize options.path and options.domain
+			// in the following expressions, otherwise they evaluate to undefined
+			// in the packed version for some reason...
+			var path = options.path ? '; path=' + (options.path) : '';
+			var domain = options.domain ? '; domain=' + (options.domain) : '';
+			var secure = options.secure ? '; secure' : '';
+			document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+		} else { // only name given, get cookie
+			var cookieValue = null;
+			if (document.cookie && document.cookie != '') {
+				var cookies = document.cookie.split(';');
+				for (var i = 0; i < cookies.length; i++) {
+					var cookie = jQuery.trim(cookies[i]);
+					// Does this cookie string begin with the name we want?
+					if (cookie.substring(0, name.length + 1) == (name + '=')) {
+						cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+						break;
+					}
+				}
+			}
+			return cookieValue;
+		}
 	};
 	
 	window.Cookie = {
@@ -81,23 +175,8 @@
 	 * @return String
 	 */
 	$.fn.serializeParam = function() {
-		var params = {};
-
-		jQuery.each(this.serializeArray(), function(index, value) {
-			if(value.name.substr(value.name.length - 2, 2) === '[]') {
-				var name = value.name.substr(0, value.name.length - 2);
-				
-				if(!params[name]) {
-					params[name] = [];
-				}
-				
-				params[name].push(value.value);
-			} 
-			else {
-				params[value.name] = value.value;
-			}
-		});
-
+		var params = $.deparam(this.serialize());
+		
 		return params;
 	};
 
